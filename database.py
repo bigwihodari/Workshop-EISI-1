ff32398caeda22ebdd3b86075050bd315d4ea1d5
bigwi.hodari <bigwihodari@gmail.com>
2025-10-08 17:20:10 +0200

    api 08_10_9

--- database.py
+++ database.py
@@ -5,9 +5,8 @@ from dotenv import load_dotenv
 import os
 
 load_dotenv()
-# print("DATABASE_URL =", os.getenv("DATABASE_URL"))
-# DATABASE_URL = os.getenv("DATABASE_URL")
-DATABASE_URL = "postgresql://postgres:admin123@localhost/workshop_eisi_1"
+
+DATABASE_URL = os.getenv("DATABASE_URL")
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
